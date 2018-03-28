f5da27ee88d45bd36ba2e20ca83084fb4195ab0b
jrcpulliam <juliet@ici3d.org>
2018-03-28 22:30:39 +0200

    draft of initial concept note, sent to AW

---
+++ epiFunctions.R
@@ -0,0 +1,33 @@
+# epiFunctions.R
+
+# For half-life of 2 weeks, prob = 1-sqrt(2)/2
+# For mean of 2 weeks, prob = 1/3
+
+exposureProfile <- function(baseline,effectiveness,prop = 1/3,recallWeek=10,maxWeek=18){
+  
+  re <- c(rep(baseline,recallWeek-1),baseline*(1-effectiveness))
+  while(length(re)<maxWeek){
+    re <- c(re,prop*re[length(re)])
+  }
+  
+  exp <- data_frame(week = 1:maxWeek, riskExp = re)
+  
+}
+
+projCurve <- function(casesGen # future cases generated through risky exposures
+                      , gamMean = meanIncGamma
+                      , gamRate = rateIncGamma
+                      , maxWeek = length(casesGen)
+                      , breaks=seq(3.5,maxWeek*7,7)
+                      , week=1:maxWeek
+                      , browse = F
+                      ){
+  if(browse) browser()
+  prop <- diff(pgamma(c(0,breaks),gamMean*gamRate,gamRate))
+  expected <- rep(0,maxWeek)
+  for(ww in week){
+    spread <- casesGen[ww]*prop
+    expected[ww:maxWeek] <- expected[ww:maxWeek] + spread[1:(maxWeek-ww+1)]
+  }
+  return(expected)
+}
---
+++ incubationDist.R
@@ -0,0 +1,80 @@
+# incubationDist.R
+require(tidyverse)
+require(bbmle)
+require(interval)
+
+fname <- '~/Dropbox\ (Personal)/SACEMA/NICD/Listeria/Goulet2013.xlsx'
+
+incDat <- (
+  readxl::read_xlsx(fname)
+)
+
+incDat <- (
+  incDat
+  %>% filter(.,ref != 25) # remove data from outbreak where source is questionable
+  %>% filter(.,ref != 27) # remove data from outbreak without individual-level information
+  %>% select(.,-c(id,country,year,ref,food))
+)
+
+# Duration of the incubation period
+perInc_min <- rep(NA,nrow(incDat))
+perInc_max <- rep(NA,nrow(incDat))
+# Add uncertainty for 'known' incubation periods
+ipKnown <- which(incDat$treatment == 'known')
+perInc_min[ipKnown] <- incDat$incPer[ipKnown] - .5
+perInc_max[ipKnown] <- incDat$incPer[ipKnown] + .5
+# Add uncertainty for left censored data
+ipLeft <- which(incDat$treatment %in% c('sens','censored'))
+perInc_min[ipLeft] <- 0
+perInc_max[ipLeft] <- incDat$incPerMax[ipLeft]
+# Add uncertainty for interval censored data
+ipOr <- which(incDat$treatment == 'or')
+perInc_min[ipOr] <- incDat$incPerMin[ipOr]
+perInc_max[ipOr] <- incDat$incPerMax[ipOr]
+
+npmle_perInc <- icfit(perInc_min,perInc_max,conf.int = T)
+
+nllGammaIcens <- function(logmean = NULL, lograte = NULL){
+  pp <- exp(c(logmean,lograte))
+  alpha <- pp[1]*pp[2]
+  lambda <- pp[2]
+  # Calculate probabilities of being in intervals
+  intervalProb <- (
+    pgamma(perInc_max
+           , shape = alpha, rate = lambda
+           , log = F) -
+      pgamma(perInc_min
+             , shape = alpha, rate = lambda
+             , log = F)
+  )
+  # Adjust for data with two exposures
+  firstExp <- (
+    pgamma(perInc_min[ipOr] + 0.5
+           , shape = alpha, rate = lambda
+           , log = F) -
+      pgamma(perInc_min[ipOr] - 0.5
+             , shape = alpha, rate = lambda
+             , log = F)
+  )
+  secondExp <- (
+    pgamma(perInc_max[ipOr] + 0.5
+           , shape = alpha, rate = lambda
+           , log = F) -
+      pgamma(perInc_max[ipOr] - 0.5
+             , shape = alpha, rate = lambda
+             , log = F)
+  )
+  intervalProb[ipOr] <- firstExp+secondExp
+  nll <- -sum(log(intervalProb))
+  return(nll)
+}
+
+initParams <- c(logmean = log(1), lograte = log(1))
+estIncGamma <- mle(nllGammaIcens,start = as.list(initParams))
+ciIncGamma <- confint(estIncGamma)
+
+ciIncGamma_Rate <- round(unname(exp(ciIncGamma[2,])),2)
+
+meanIncGamma <- round(unname(exp(coef(estIncGamma)[1])),2)
+rateIncGamma <- round(unname(exp(coef(estIncGamma)[2])),2)
+ciIncGamma <- round(unname(exp(ciIncGamma[1,])),2)
